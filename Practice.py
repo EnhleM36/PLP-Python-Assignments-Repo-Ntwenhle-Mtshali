e26caf8252fe1b126c63436bbfb884cd482b0c81
EnhleM36 <nana.mtshali3@gmail.com>
2025-03-14 18:45:16 +0200

    git push origin master

--- Practice.py
+++
@@ -1,5 +0,0 @@
-
-print("A Message for You! But first,")
-user_input = input("Enter UserName")
-color_input = input("Enter Favorite_Color:")
-print("Hello," + user_input + "!" "Your favorite color," + color_input +",is awesome!")
