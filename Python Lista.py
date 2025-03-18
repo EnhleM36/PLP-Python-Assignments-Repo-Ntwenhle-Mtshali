39ef9cb454c26ab94ea2cfb321af67dfe8363be9
EnhleM36 <nana.mtshali3@gmail.com>
2025-03-18 22:06:42 +0200

    Python Assignment W2

---
+++ Python Lista.py
@@ -0,0 +1,13 @@
+my_list =[]
+my_list = [10, 20, 30, 40]
+print(my_list)
+my_list.insert(2, 15)
+print(my_list)
+my_list2 = [50, 60, 70]
+my_list.extend(my_list2)
+print(my_list)
+my_list.remove(70)
+print(my_list)
+my_list.sort()
+print(my_list)
+print(my_list[3])
